6919804236a530fb7ec8387a69bd1d8d7f53f958
Surya Prakash <suryaskasccollege@gmail.com>
2025-12-10 10:29:01 +0530

    Add files via upload

---
+++ prgm_2.R
@@ -0,0 +1,11 @@
+c<-16
+print(paste("the square root of c is:",sqrt(c)))
+print(paste("the log of c is:",log(c)))
+print(paste("the sin value of c is:",sin(c)))
+d<-14
+print(paste("the the sum of the number is:",sum(c+d)))
+print(paste("the minimum value is:",min(c:d)))
+print(paste("the maximum value is:",max(c:d)))
+e<-c(2,12,45,22,19)
+sort(e)
+summary(e)
