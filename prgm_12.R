1f276053150d2b77058708a3662b94038942ae4c
Surya Prakash <suryaskasccollege@gmail.com>
2026-02-11 12:11:17 +0530

    Add files via upload

---
+++ prgm_12.R
@@ -0,0 +1,37 @@
+library(ggplot2)
+
+data(mtcars)
+mtcars <- as.data.frame(mtcars)
+
+data_subset <- mtcars[, c("mpg", "hp", "wt")]
+scaled_data <- scale(data_subset)
+
+set.seed(123)
+
+wss <- sapply(1:10, function(k) {
+  kmeans(scaled_data, centers = k, nstart = 25)$tot.withinss
+})
+
+plot(
+  1:10, wss,
+  type = "b",
+  pch = 19,
+  frame = FALSE,
+  xlab = "Number of clusters (k)",
+  ylab = "Total within cluster sum of squares",
+  main = "Elbow Method"
+)
+
+set.seed(123)
+kmeans_model <- kmeans(scaled_data, centers = 3, nstart = 25)
+
+mtcars$cluster <- as.factor(kmeans_model$cluster)
+
+ggplot(mtcars, aes(x = mpg, y = hp, color = cluster)) +
+  geom_point(size = 3) +
+  labs(
+    title = "K-means Clustering on mtcars Dataset",
+    x = "Miles Per Gallon",
+    y = "Horsepower"
+  ) +
+  theme_minimal()
